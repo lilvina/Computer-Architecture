be7be386113dc28f75b195552ebf186c268110d9
lilvina <davinaleo2007@yahoo.com>
2019-07-24 14:55:22 -0700

    day2 MVP done

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,6 +5,18 @@
 import sys
 from cpu import *
 
+file_open = sys.argv[1]
+
+print(f'Opening File: {file_open}')
+
+file = open(file_open, 'r')
+instructions = []
+
+for line in file:
+    if line[0] != '#':
+        if line != '\n':
+            instructions.append(int(line[:8], 2))
+
 cpu = CPU()
 
 cpu.load()
